853651be54e55939a21c8fe24847f12300c22e57
Ankit <ankit@zivost.com>
2021-03-12 23:38:41 +0530

    canvas skeleton

--- src/pages/welcome/welcome.tsx
+++ src/pages/welcome/welcome.tsx
@@ -1,20 +1,41 @@
 import {useAuth0} from '@auth0/auth0-react'
+import {Container, Grid, makeStyles} from '@material-ui/core'
 
 export const Welcome: React.FC = () => {
   const {user, isAuthenticated} = useAuth0()
+  const classes = useStyles()
+
+  if (!isAuthenticated) return <h2>Welcome Please Loggin.</h2>
 
   return (
-    <div>
-      <h2>Home</h2>
-      {isAuthenticated ? (
-        <div>
-          <h2>{user.name}</h2>
-
-          <p>{user.email}</p>
-        </div>
-      ) : (
-        <div>Welcome to Prolly, please login</div>
-      )}
-    </div>
+    <Container>
+      <Grid container spacing={3}>
+        <Grid className={classes.toolbar} item xs={12}>
+          Tool Bar
+        </Grid>
+        <Grid className={classes.canvas} item xs={12} md={9}>
+          Canvas
+        </Grid>
+        <Grid className={classes.filter} item xs={12} md={3}>
+          Filter side bar
+        </Grid>
+      </Grid>
+    </Container>
   )
 }
+
+const useStyles = makeStyles(theme => {
+  return {
+    toolbar: {
+      outline: `1px dotted ${theme.palette.error.light}`,
+    },
+    canvas: {
+      outline: `1px dotted ${theme.palette.error.light}`,
+      minHeight: '70vh',
+    },
+    filter: {
+      outline: `1px dotted ${theme.palette.error.light}`,
+      minHeight: '70vh',
+    },
+  }
+})
